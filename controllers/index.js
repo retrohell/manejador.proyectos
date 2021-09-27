a2757b31f51f70514eecf51b3d1ca7a9eefbaf00
IRVIN DANIEL CAMPOS VALENZUELA <a329762@uach.mx>
2021-09-26 18:31:30 -0600

    Views para login, funciones index, funciones basicas

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,18 @@ function home (req, res, next) {
     res.render('index', { title: 'Proyecto Web platforms' });
 }
 
+function login (req, res, next) {
+    res.render('index', { title: 'Proyecto Web platforms Login' });
+}
+function register (req, res, next) {
+    res.render('index', { title: 'Proyecto Web platforms Register' });
+}
+function forgot (req, res, next) {
+    res.render('index', { title: 'Proyecto Web platforms Forgot' });
+}
 module.exports = {
-    home
+    home,
+    login,
+    register,
+    forgot,
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/:id', controller.getAll);
+
+router.post('/', controller.create);
+
+router.put('/:id', controller.replace);
+
+router.patch('/:id', controller.edit);
+
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
