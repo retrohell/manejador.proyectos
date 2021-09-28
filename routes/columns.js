84c7894d6526d674ab1ea35618164eebd972525b
Ericka Bermúdez <erickabdz@gmail.com>
2021-09-27 21:17:23 -0600

    Added columns

---
+++ routes/columns.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/columns');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/:id', controller.get);
+router.get('/', controller.getAll);
+router.post('/', controller.create);
+router.put('/:id', controller.replace);
+router.patch('/:id', controller.edit);
+router.delete('/:id', controller.destroy);
+
+module.exports = router;
