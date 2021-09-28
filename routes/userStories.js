4b761e020ce6bfde7c5eb765188af2941902cb3f
Ericka Bermúdez <erickabdz@gmail.com>
2021-09-27 21:12:30 -0600

    Added user stories

---
+++ routes/userStories.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/userStories');
+
+/* GET team members listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/', controller.getAll);
+router.get('/:id', controller.getByID);
+router.get('/getUserByRole', controller.getByColumn);
+router.post('/', controller.create);
+router.put('/:id', controller.replace);
+router.patch('/:id', controller.edit);
+router.patch('/validate/:id', controller.validate);
+router.delete('/:id', controller.destroy);
+
+module.exports = router;
+
